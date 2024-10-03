e3034003ec16d388a477465c3eef24d24feee5df
Rishabh Pratap Singh <rishu1014@Rishabhs-MacBook-Pro.local>
2024-10-03 16:08:30 -0700

    boiler_plate

---
+++ FirstAppTemplateTests/FirstAppTemplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTemplateTests.swift
+//  FirstAppTemplateTests
+//
+//  Created by Rishabh Pratap Singh on 03/10/24.
+//
+
+import Testing
+@testable import FirstAppTemplate
+
+struct FirstAppTemplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ FirstAppTemplateUITests/FirstAppTemplateUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FirstAppTemplateUITestsLaunchTests.swift
+//  FirstAppTemplateUITests
+//
+//  Created by Rishabh Pratap Singh on 03/10/24.
+//
+
+import XCTest
+
+final class FirstAppTemplateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
